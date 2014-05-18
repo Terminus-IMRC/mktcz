74725a15fa97dbc9cd3b4c491ffa03c7c2fcdeae
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-05-18 21:19:51 +0900

    initial commit

---
+++ build.sh
@@ -0,0 +1,49 @@
+#!/bin/sh -ex
+TCZNAME=openmpi
+PKGTARGZ=openmpi-1.8.1.tar.gz
+PKGDIR=$(basename "$PKGTARGZ" .tar.gz)
+CONFIGURE_OPTS='--prefix=/usr/local CFLAGS=-march=armv6'
+MAKE_OPTS=''
+
+if ! test -f $TCZNAME.tcz.info; then
+	echo "error: $TCZNAME.tcz.info: no such file"
+	exit 1
+fi
+
+if ! fgrep -q XXXXX $TCZNAME.tcz.info; then
+	echo "error: $TCZNAME.tcz.info: does not contain size substitution"
+	exit 1
+fi
+
+if echo $PKGDIR | fgrep -q /; then
+	echo "error: multi-level package directory is not supported yet"
+	exit 1
+fi
+
+rm -rf $PKGDIR
+tar xf $PKGTARGZ
+cd $PKGDIR
+
+./configure $CONFIGURE_OPTS >log.c.1 2>log.c.2
+make $MAKE_OPTS >log.m.1 2>log.m.2
+rm -rf ../dest
+mkdir -p ../dest
+make install DESTDIR=../dest
+
+cd ../dest
+find * -not -type d >../$TCZNAME.tcz.list
+cd ..
+
+rm -f $TCZNAME.tcz
+mksquashfs dest $TCZNAME.tcz
+
+sed -i "s/XXXXX/$(\du -h $TCZNAME.tcz | awk '{print $1}')/" $TCZNAME.tcz.info
+
+md5sum $TCZNAME.tcz >$TCZNAME.tcz.md5.txt
+
+zsyncmake $TCZNAME.tcz
+
+rm -f $TCZNAME.tar.gz
+tar czf $TCZNAME.tar.gz $TCZNAME.tcz $TCZNAME.tcz.info $TCZNAME.tcz.list $TCZNAME.tcz.md5.txt $TCZNAME.tcz.zsync $PKGTARGZ $0
+
+#echo -e 'tinycore\ntinycore' | bcrypt $NAME.tar.gz
