3ed6a1c9738973f535c1a3e94224a4d5ea3c8317
Terminus <i.can.speak.c.and.basic@gmail.com>
2014-06-01 23:56:53 +0900

    rewrite with lots of improves

--- build.sh
+++ build.sh
@@ -1,19 +1,26 @@
 #!/bin/sh -ex
-TCZNAME=openmpi
-PKGTARGZ=openmpi-1.8.1.tar.gz
-PKGDIR=$(basename "$PKGTARGZ" .tar.gz)
-CONFIGURE_OPTS='--prefix=/usr/local CFLAGS="-Os -pipe" CXXFLAGS="-Os -pipe" LDFLAGS="-Wl,-O1"'
-MAKE_OPTS=''
+# mktcz.sh -- for making extensions for TinyCoreLinux
+# (c) Akane, alias Sugizaki Yukimasa
 
-if ! test -f $TCZNAME.tcz.info; then
-	echo "error: $TCZNAME.tcz.info: no such file"
+. ./build.rc
+
+if test -z "$TCZNAME"; then
+	echo "error: \$TCZNAME is empty"
+	exit 1
+fi
+if test -z "$PKGTARGZ"; then
+	echo "error: \$PKGTARGZ is empty"
 	exit 1
 fi
 
-if ! fgrep -q XXXXX $TCZNAME.tcz.info; then
-	echo "error: $TCZNAME.tcz.info: does not contain size substitution"
-	exit 1
-fi
+export_if_not_empty(){
+	if test -n "$(eval echo \$$1)"; then
+		export $1
+	fi
+}
+export_if_not_empty CFLAGS
+export_if_not_empty CXXFLAGS
+export_if_not_empty LDFLAGS
 
 if echo $PKGDIR | fgrep -q /; then
 	echo "error: multi-level package directory is not supported yet"
@@ -26,13 +33,13 @@ fi
 cd $PKGDIR
 
 if ! test -f log.c.done; then
-	rm -f log.m.done
 	if test -n "$CONFIGURE_OPTS"; then
 		./configure "$CONFIGURE_OPTS"
 	else
 		./configure
 	fi >log.c.1 2>log.c.2
 	touch log.c.done
+	rm -f log.m.done
 fi
 
 if ! test -f log.m.done; then
@@ -47,29 +54,94 @@ if ! test -f log.m.done; then
 	touch log.m.done
 fi
 
-cd ../dest
-
-#TODO: smarter way needed
-for f in $(find . -not -type d); do
-	if file $f | grep executable -q; then
-		strip --strip-all $f
+add_file_as_licence_if_exists(){
+	if test -f $1; then
+		mkdir -p ../dest/usr/local/share/licences/$TCZNAME
+		cp $1 ../dest/usr/local/share/licences/$TCZNAME/
 	fi
-done
+}
+add_file_as_licence_if_exists LICENCE
+add_file_as_licence_if_exists LICENSE
+add_file_as_licence_if_exists COPYING
 
-find * -not -type d >../$TCZNAME.tcz.list
 cd ..
 
-rm -f $TCZNAME.tcz
-mksquashfs dest $TCZNAME.tcz
+if ! test -f log.strip.done; then
+	cd dest
+	#TODO: smarter way is needed
+	for f in $(find . -type f); do
+		if file $f | grep executable | grep stripped -wq; then
+			strip --strip-all $f
+		fi
+	done
+	cd ..
+	touch log.strip.done
+fi
 
-sed -i "s/XXXXX/$(\du -h $TCZNAME.tcz | awk '{print $1}')/" $TCZNAME.tcz.info
+if ! test -f log.move.done; then
+	cd dest
 
-md5sum $TCZNAME.tcz >$TCZNAME.tcz.md5.txt
+	move_to_another_if_exists(){
+		if test -d "$1"; then
+			mkdir -p ../dest-$2/${1%/*}
+			mv $1 ../dest-$2/${1%/*}/
+		fi
+	}
+	move_to_another_if_exists usr/local/share/man doc
+	move_to_another_if_exists usr/local/share/doc doc
+	move_to_another_if_exists usr/local/include dev
+	move_to_another_if_exists usr/local/share/locale locale
 
-rm -rf $TCZNAME.tcz.zsync
-zsyncmake $TCZNAME.tcz
+	cd ..
+	touch log.move.done
+fi
 
-rm -f $TCZNAME.tar.gz
-tar czf $TCZNAME.tar.gz $TCZNAME.tcz $TCZNAME.tcz.info $TCZNAME.tcz.list $TCZNAME.tcz.md5.txt $TCZNAME.tcz.zsync $(test -f "$TCZNAME.tcz.dep" && echo $TCZNAME.tcz.dep || echo) $PKGTARGZ $0
+if test -d dest-doc; then
+	export HAVE_doc=1
+fi
+if test -d dest-dev; then
+	export HAVE_dev=1
+fi
+if test -d dest-locale; then
+	export HAVE_locale=1
+fi
 
-#echo -e 'tinycore\ntinycore' | bcrypt $NAME.tar.gz
+pack_all(){
+	if ! test -f $1.tcz.info; then
+		echo "error: $1.tcz.info: no such file"
+		exit 1
+	fi
+
+	cd $2
+	find * -not -type d >../$1.tcz.list
+	cd ..
+
+	rm -f $1.tcz
+	mksquashfs $2 $1.tcz
+
+	sed -i "s/\(Size:[ \t]*\).*/\1$(\du -h $1.tcz | awk '{print $1}')/" $1.tcz.info
+
+	md5sum $1.tcz >$1.tcz.md5.txt
+
+	rm -f $1.tcz.zsync
+	zsyncmake $1.tcz
+
+	rm -f $1.tar.gz
+	tar czf $1.tar.gz $1.tcz $1.tcz.info $1.tcz.list $1.tcz.md5.txt $1.tcz.zsync $(test -f "$1.tcz.dep" && echo $1.tcz.dep || echo) $PKGTARGZ $(basename $0) build.rc
+
+	#echo -e 'tinycore\ntinycore' | bcrypt $1.tar.gz
+}
+pack_all $TCZNAME dest
+pack_all_co(){
+	if test -n "$(eval echo \$HAVE_$1)"; then
+		rm -f $TCZNAME-$1.tcz.dep
+		if test -f $TCZNAME.tcz.dep; then
+			cp $TCZNAME.tcz.dep $TCZNAME-$1.tcz.dep
+		fi
+		echo $TCZNAME.tcz >>$TCZNAME-$1.tcz.dep
+		pack_all $TCZNAME-$1 dest-$1
+	fi
+}
+pack_all_co doc
+pack_all_co dev
+pack_all_co locale
